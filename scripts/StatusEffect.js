11c10a9acebf0db949ac9857a7fd41d870bf2ea7
Seraphalim4404 <nickolosebastian@gmail.com>
2026-05-06 19:33:24 +0800

    Update StatusEffect.js

--- scripts/StatusEffect.js
+++ scripts/StatusEffect.js
@@ -8,7 +8,7 @@ function StatusEffect{
   
 
 
-  if(!Vars.headless && this.effect !== Fx.none && !unit.inFogTo(Vars.player.team()) && unit.health / unit.maxHealth <= 0.25) {
+  if(!Vars.testing && this.effect !== Fx.none && !unit.inFogTo(Vars.player.team()) && unit.health / unit.maxHealth <= 0.25) {
     Tmp.v1.rnd(Mathf.range(unit.type.hitSize * 0.5));
     this.effect.at(unit.x + Tmp.v1.x, unit.y + Tmp.v1.y, 0.0, this.color, this.parentizeEffect ? unit : null);
 };
