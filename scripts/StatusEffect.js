f149605bb2da1dcbd818fb0621bb01965f8cfb7e
Seraphalim4404 <nickolosebastian@gmail.com>
2026-05-09 00:02:45 +0800

    Update StatusEffect.js

--- scripts/StatusEffect.js
+++ scripts/StatusEffect.js
@@ -1,17 +1,19 @@
-Events.run(Trigger.update, () => {
-   
+Events.run(Trigger.update, setStatus);
+
+function setStatus() {
     if(Vars.state.isMenu()) return;
 
-    if(Mathf.chance(0.02) && Vars.player.unit()) { 
-        Vars.player.unit().apply(StatusEffects."lost-restrained", 60);
+    if(Mathf.chance(0.02) && Vars.player.unit() != null) { 
+        let restr = Vars.content.getByName(ContentType.status, "lost-restrained");
+        if(restr) Vars.player.unit().apply(restr, 300);
+    };
+
+    if(!Vars.headless) {
+        Groups.unit.each(unit => {
+            if(!unit.inFogTo(Vars.player.team()) && unit.healthf() <= 0.25) {
+                Tmp.v1.rnd(Mathf.range(unit.type.hitSize * 0.5));
+                Fx.fireSmoke.at(unit.x + Tmp.v1.x, unit.y + Tmp.v1.y);
+            }
+        });
     }
-
-    Groups.unit.each(unit => {
-
-        if(!Vars.headless && unit.healthf() <= 0.25) {
-            Tmp.v1.rnd(unit.type.hitSize * 0.5);
-            
-            Fx.burning.at(unit.x + Tmp.v1.x, unit.y + Tmp.v1.y);
-        }
-    });
-});
+}
