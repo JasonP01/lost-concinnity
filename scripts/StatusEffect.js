55b0b88cc9be27f97776245f9140042d1bffb6fc
Seraphalim4404 <nickolosebastian@gmail.com>
2026-05-08 23:55:35 +0800

    Update StatusEffect.js

--- scripts/StatusEffect.js
+++ scripts/StatusEffect.js
@@ -3,7 +3,7 @@ Events.run(Trigger.update, () => {
     if(Vars.state.isMenu()) return;
 
     if(Mathf.chance(0.02) && Vars.player.unit()) { 
-        Vars.player.unit().apply(StatusEffects.restrained, 60);
+        Vars.player.unit().apply(StatusEffects.lost-restrained, 60);
     }
 
     Groups.unit.each(unit => {
