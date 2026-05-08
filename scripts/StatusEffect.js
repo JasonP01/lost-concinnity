ca08656c86035b74983da7781e72dc6e34c327bc
Seraphalim4404 <nickolosebastian@gmail.com>
2026-05-08 23:08:44 +0800

    Update StatusEffect.js

--- scripts/StatusEffect.js
+++ scripts/StatusEffect.js
@@ -1,10 +1,12 @@
 Events.run(Trigger.update, e => { if(Mathf.chance(2)) { Vars.player.unit().apply(Vars.content.get(lost-restrained), 60)  }});
-
-  if(!Vars.headless && this.effect !== Fx.none && !unit.inFogTo(Vars.player.team()) && unit.health / unit.maxHealth <= 0.25) {
+  try {
+    if(!Vars.headless && this.effect !== Fx.none && !unit.inFogTo(Vars.player.team()) && unit.health / unit.maxHealth <= 0.25) {
     Tmp.v1.rnd(Mathf.range(unit.type.hitSize * 0.5));
     this.effect.at(unit.x + Tmp.v1.x, unit.y + Tmp.v1.y, 0.0, this.color, this.parentizeEffect ? unit : null);
-  };
-
+    };
+  } catch(e){
+  Vars.ui.showInfoToast(e + "-file", 5);
+  }
 module.exports = {
   StatusEffect: StatusEffect
 }
